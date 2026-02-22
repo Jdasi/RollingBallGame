5c1cf635dc53c37a2e1a2b77f93c8f6960dd3c80
silvacore@hotmail.co.uk <silvacore@hotmail.co.uk>
2026-02-22 16:28:42 +0000

    add camera adjustment during aiming mode

--- Source/RollingBallGame/ActorComponents/LaunchAimComponent.cpp
+++ Source/RollingBallGame/ActorComponents/LaunchAimComponent.cpp
@@ -2,6 +2,8 @@
 
 #include "LaunchAimComponent.h"
 #include "RollingBallGameCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "Subsystems/TimeDilationSubsystem.h"
 
 ULaunchAimComponent::ULaunchAimComponent()
@@ -9,14 +11,21 @@ ULaunchAimComponent::ULaunchAimComponent()
     PrimaryComponentTick.bCanEverTick = true;
 }
 
-void ULaunchAimComponent::StartAim() const
+void ULaunchAimComponent::StartAim()
 {
-    GetWorld()->GetSubsystem<UTimeDilationSubsystem>()->RequestDilation("RollingBall", 0.4f);
+    if (!AimRequested)
+    {
+        SetTimeDilationActive(true);
+    }
+
+    AimRequested = true;
+    AimActive = true;
 }
 
-void ULaunchAimComponent::EndAim() const
+void ULaunchAimComponent::EndAim()
 {
-    GetWorld()->GetSubsystem<UTimeDilationSubsystem>()->ClearRequest("RollingBall");
+    AimRequested = false;
+    ExitTimer = ExitAimDelay;
 }
 
 void ULaunchAimComponent::BeginPlay()
@@ -26,10 +35,64 @@ void ULaunchAimComponent::BeginPlay()
     const ARollingBallGameCharacter* RollingBall = Cast<ARollingBallGameCharacter>(GetOwner());
     Camera = RollingBall->GetCamera();
     CameraBoom = RollingBall->GetCameraBoom();
+
+    InitialOffset = CameraBoom->SocketOffset;
+    InitialFov = Camera->FieldOfView;
+    InitialCameraLag = CameraBoom->CameraLagSpeed;
+    InitialCameraRotationLag = CameraBoom->CameraRotationLagSpeed;
 }
 
 void ULaunchAimComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    // time dilation shouldn't affect camera lerp
+    DeltaTime = FApp::GetDeltaTime();
+
+    if (AimRequested)
+    {
+        CameraBoom->SocketOffset = FMath::VInterpTo(CameraBoom->SocketOffset, OffsetLerpTarget, DeltaTime, OffsetLerpSpeed);
+        CameraBoom->CameraLagSpeed = FMath::FInterpTo(CameraBoom->CameraLagSpeed, CameraLagLerpTarget, DeltaTime, CameraLagLerpSpeed);
+        CameraBoom->CameraRotationLagSpeed  = FMath::FInterpTo(CameraBoom->CameraRotationLagSpeed, CameraLagLerpTarget, DeltaTime, CameraLagLerpSpeed);
+        Camera->FieldOfView = FMath::FInterpTo(Camera->FieldOfView, FovLerpTarget, DeltaTime, FovLerpSpeed);
+    }
+    else
+    {
+        if (AimActive)
+        {
+            if (ExitTimer > 0)
+            {
+                ExitTimer -= DeltaTime;
+
+                if (ExitTimer <= 0.0f)
+                {
+                    AimActive = false;
+                    SetTimeDilationActive(false);
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        CameraBoom->SocketOffset = FMath::VInterpTo(CameraBoom->SocketOffset, InitialOffset, DeltaTime, OffsetLerpSpeed);
+        CameraBoom->CameraLagSpeed = FMath::FInterpTo(CameraBoom->CameraLagSpeed, InitialCameraLag, DeltaTime, CameraLagLerpSpeed);
+        CameraBoom->CameraRotationLagSpeed  = FMath::FInterpTo(CameraBoom->CameraRotationLagSpeed, InitialCameraRotationLag, DeltaTime, CameraLagLerpSpeed);
+        Camera->FieldOfView = FMath::FInterpTo(Camera->FieldOfView, InitialFov, DeltaTime, FovLerpSpeed);
+    }
 }
 
+void ULaunchAimComponent::SetTimeDilationActive(bool Active) const
+{
+    const FName RequesterId = FName("RollingBall");
+
+    if (Active)
+    {
+        GetWorld()->GetSubsystem<UTimeDilationSubsystem>()->RequestDilation(RequesterId, TimeDilation);
+    }
+    else
+    {
+        GetWorld()->GetSubsystem<UTimeDilationSubsystem>()->ClearRequest(RequesterId);
+    }
+}
--- Source/RollingBallGame/ActorComponents/LaunchAimComponent.h
+++ Source/RollingBallGame/ActorComponents/LaunchAimComponent.h
@@ -15,10 +15,37 @@ class ROLLINGBALLGAME_API ULaunchAimComponent : public UActorComponent
     GENERATED_BODY()
 
 public:
+    UPROPERTY(EditAnywhere, Category="Settings")
+    float TimeDilation = 0.4f;
+
+    UPROPERTY(EditAnywhere, Category="Settings")
+    float ExitAimDelay = 0.5f;
+
+    UPROPERTY(EditAnywhere, Category="Lerp Targets")
+    FVector OffsetLerpTarget = FVector::ZeroVector;
+
+    UPROPERTY(EditAnywhere, Category="Lerp Targets")
+    float FovLerpTarget = 50.0f;
+
+    UPROPERTY(EditAnywhere, Category="Lerp Targets")
+    float CameraLagLerpTarget = 1.0f;
+
+    UPROPERTY(EditAnywhere, Category="Lerp Speeds")
+    float OffsetLerpSpeed = 10.0f;
+
+    UPROPERTY(EditAnywhere, Category="Lerp Speeds")
+    float FovLerpSpeed = 10.0f;
+
+    UPROPERTY(EditAnywhere, Category="Lerp Speeds")
+    float CameraLagLerpSpeed = 10.0f;
+
+    UPROPERTY(EditAnywhere, Category="Lerp Speeds")
+    float CameraRotationLagLerpSpeed = 10.0f;
+
     ULaunchAimComponent();
 
-    void StartAim() const;
-    void EndAim() const;
+    void StartAim();
+    void EndAim();
 
 protected:
     virtual void BeginPlay() override;
@@ -30,4 +57,14 @@ private:
 
     UPROPERTY()
     USpringArmComponent* CameraBoom = nullptr;
+
+    void SetTimeDilationActive(bool Active) const;
+
+    FVector InitialOffset = FVector::ZeroVector;
+    float InitialFov = 0.0f;
+    float InitialCameraLag = 0.0f;
+    float InitialCameraRotationLag = 0.0f;
+    float ExitTimer = 0.0f;
+    bool AimActive = false;
+    bool AimRequested = false;
 };
