3676f8ad4ae26220386485b0554ee0721f0bc80d
silvacore@hotmail.co.uk <silvacore@hotmail.co.uk>
2026-02-22 01:20:47 +0000

    add TimeDilationSubsystem

---
+++ Source/RollingBallGame/Subsystems/TimeDilationSubsystem.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TimeDilationSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+
+void UTimeDilationSubsystem::RequestDilation(const FName& Requester, const float Amount)
+{
+    if (Amount == 1.0f)
+    {
+        ClearRequest(Requester);
+    }
+    else
+    {
+        auto& Val = Requests.FindOrAdd(Requester);
+        Val = Amount;
+
+        RecalculateCurrentDilation();
+    }
+}
+
+void UTimeDilationSubsystem::ClearRequest(const FName& Requester)
+{
+    Requests.Remove(Requester);
+    RecalculateCurrentDilation();
+}
+
+void UTimeDilationSubsystem::ClearAllRequests()
+{
+    Requests.Empty();
+    RecalculateCurrentDilation();
+}
+
+void UTimeDilationSubsystem::OnWorldEndPlay(UWorld& InWorld)
+{
+    Super::OnWorldEndPlay(InWorld);
+    ClearAllRequests();
+}
+
+void UTimeDilationSubsystem::RecalculateCurrentDilation()
+{
+    if (Requests.IsEmpty())
+    {
+        CurrentDilation = 1.0f;
+    }
+    else
+    {
+        CurrentDilation = 0.0f;
+
+        for (const auto& Request : Requests)
+        {
+            CurrentDilation = FMath::Max(CurrentDilation, Request.Value);
+        }
+    }
+
+    // TODO - lerp to new value?
+    UGameplayStatics::SetGlobalTimeDilation(GetWorld(), CurrentDilation);
+}
---
+++ Source/RollingBallGame/Subsystems/TimeDilationSubsystem.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "TimeDilationSubsystem.generated.h"
+
+UCLASS()
+class ROLLINGBALLGAME_API UTimeDilationSubsystem : public UWorldSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    /** Request slo-mo or overwrite a request. 0 = slow, 1 = normal, 2 = fast. */
+    void RequestDilation(const FName& Requester, const float Amount);
+    void ClearRequest(const FName& Requester);
+    void ClearAllRequests();
+
+private:
+    TMap<FName, float> Requests;
+    float CurrentDilation = 0;
+
+    virtual void OnWorldEndPlay(UWorld& InWorld) override;
+    void RecalculateCurrentDilation();
+
+};
